fd294f9e9818ac938af84ee178eb2ad06d869c73
JiaLee0707 <s2018s37@e-mirim.hs.kr>
2020-02-13 01:46:07 +0900

    달력&내용쓰기&색깔 완성&로그인

---
+++ app/src/main/java/kr/hs/emirim/s2018s37/ru_ok/JoinActivity.java
@@ -0,0 +1,19 @@
+package kr.hs.emirim.s2018s37.ru_ok;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+import java.util.regex.Pattern;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class JoinActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join);
+    }
+
+
+}
---
+++ app/src/main/java/kr/hs/emirim/s2018s37/ru_ok/WriteDairy.java
@@ -0,0 +1,42 @@
+package kr.hs.emirim.s2018s37.ru_ok;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WriteDairy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_write_dairy);
+
+        View.OnClickListener save_listener = new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                //DB에 저장
+                Toast.makeText(WriteDairy.this, "저장되었습니다.", Toast.LENGTH_LONG).show();
+                Intent cancel_intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(cancel_intent);
+            }
+        };
+
+        View.OnClickListener cancel_listener = new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent cancel_intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(cancel_intent);
+            }
+        };
+
+        Button save_btn = (Button) findViewById(R.id.save);
+        save_btn.setOnClickListener(save_listener);
+
+        Button cancel_btn = (Button) findViewById(R.id.cancel);
+        cancel_btn.setOnClickListener(cancel_listener);
+    }
+}
